e33ee4eadaeab2a94ea998a73883e346898ef84c
Samuel Gajdos <gajdos.samuel@gmail.com>
2026-06-19 23:39:59 +0200

    Added slf4j logger dependency

--- src/main/java/io/github/samGajdos/converter/App.java
+++ src/main/java/io/github/samGajdos/converter/App.java
@@ -1,10 +1,20 @@
 package io.github.samGajdos.converter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * Hello world!
+ * JSON to XML Converter
+ * Author: Samuel Gajdos
+ * Date: June 2026
  */
 public class App {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        LOGGER.info("Hello from Logger!");
+
+        //TODO Use Apache Camel XJ to convert
     }
 }
